912eeb6a24d252dc649e09fe8fa8412fea01fb3f
jdkramhoft <jdkramhoft@gmail.com>
2026-02-20 19:10:16 +0100

    dunder maining... i think

--- file.py
+++ file.py
@@ -2,3 +2,9 @@ print("Hello, World!")
 print("This is a sample Python file.")
 print("Hello, I am also here")
 # uihgyugyu
+
+
+
+# yeah but actually this change is cool
+if __name__ == "__main__":
+    print("yup we sure are main")
